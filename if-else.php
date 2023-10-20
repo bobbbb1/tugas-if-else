77cf2f9a92c8c4d9f5755d2a468b4a27548f6481
bobb.1 <137739660+bobbbb1@users.noreply.github.com>
2023-10-20 13:45:32 +0700

    Create if-else.php

---
+++ if-else.php
@@ -0,0 +1,9 @@
+<?php
+$nilai = 75; // Ganti nilai ini sesuai kebutuhan
+
+if ($nilai >= 70) {
+    echo "Anda lulus dengan nilai $nilai.";
+} else {
+    echo "Anda tidak lulus dengan nilai $nilai.";
+}
+?>
